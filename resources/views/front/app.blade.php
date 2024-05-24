c244c8872b30e2dfae006986442d1014c3db6b83
Noyon Ahammed Sojon <ahammedsojon000@gmail.com>
2024-05-24 11:17:04 +0600

    app initialized

---
+++ resources/views/front/app.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Multipurpose Registration</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <h2 class="text-2xl text-red-500">Hello</h2>
+</body>
+</html>
